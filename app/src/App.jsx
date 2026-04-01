7ada509495f3a62cd9119f30e8dde13f086a6c88
shyamdigitalit <donotreply.digital@shyamgroup.com>
2026-04-01 16:33:09 +0530

    new style project setup under process

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,24 +1,121 @@
-import { useEffect } from 'react';
-import Router from './routes/Router';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { checkAuth } from './redux/slices/authSlice';
-import Login from './pages/Login';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 
-const App = () => {
-  const dispatch = useDispatch();
-  const { loading } = useSelector(state => state.auth);
+function App() {
+  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    dispatch(checkAuth());
-  }, [dispatch]);
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
 
-  if (loading) return <div className="loader-css">
-        <img src="./loader.svg" width={200}  alt="" />
-      </div>;
+      <div className="ticks"></div>
 
-  // if (!isAuthenticated) return location.assign('/login');
-  if (!loading) return <Router />;
-};
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-export default App;
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,50 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load .env files based on the current `mode`
-  const env = loadEnv(mode, __dirname, '')
-
-  const venv = env.VITE_ENV || 'dev'
-  const appenv = env.VITE_APP_ENV || 'quality'
-
-  const portDetails = {
-    quality: Number(env.VITE_APPPORT_QAS) || 3027,
-    production: Number(env.VITE_APPPORT_PRD) || 3026,
-  }
-  const apiUrl = {
-    dev: {
-      quality: String(env.VITE_APIURL_DEVQ).toLowerCase(),
-      production: String(env.VITE_APIURL_DEVP).toLowerCase(),
-    },
-    live: {
-      quality: String(env.VITE_APIURL_QAS).toLowerCase(),
-      production: String(env.VITE_APIURL_PRD).toLowerCase(),
-    }
-  }
-
-  return {
+export default defineConfig({
   plugins: [react()],
-  server: {
-    port: portDetails[appenv] || 3027,
-    host: true,
-    proxy: {
-      // Proxy /api/* requests to backend during development
-      '/api': {
-        target: apiUrl[venv][appenv] || 'http://localhost:5027',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-}})
+})
